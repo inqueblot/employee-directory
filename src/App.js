70bc480cea70f5072281b8484495740da929fd26
inqueblot <notminebydesign@gmail.com>
2021-03-04 17:41:41 -0600

    sorted out data/API,  add styling to table

--- src/App.js
+++ src/App.js
@@ -4,20 +4,69 @@ import './App.css';
 import axios from "axios"
 
 function App() {
-  const [employeeData, setEmployeeData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const result = await axios("https://randomuser.me/api/");
-      console.log(result.data)
-      setEmployeeData(result.data)
-      console.log(employeeData)
+      const result = await axios("https://randomuser.me/api/?results=5");
+      console.log(result.data.results)
+      setData(result.data.results)
+
+
     })();
   }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "EMPLOYEE DIRECTORY",
+        columns: [
+          {
+            Header: "Photo",
+            accessor: "picture.thumbnail",
+            Cell: ({ cell: { value } }) => {
+              return (
+                <img src={value} alt="head shot" />
+              );
+            }
+          },
+          {
+            Header: "First Name",
+            accessor: "name.first"
+          },
+          {
+            Header: "Last Name",
+            accessor: "name.last"
+          },
+          {
+            Header: "Phone #",
+            accessor: "phone"
+          },
+          {
+            Header: "Address",
+            accessor: "location",
+            Cell: ({ cell: { value } }) => {
+              console.log(value)
+              return (
+                <text>
+                  {`${value.street.number} ${value.street.name}\n${value.city}, ${value.state} ${value.postcode}\n${value.country}`}
+                </text>
+              )
+            }
+          },
+          {
+            Header: ""
+          }
+        ]
+      }
+    ], []
+  )
+
 
 
   return (
     <div className="App">
+      {/* "hello world" */}
       <Table columns={columns} data={data} />
     </div>
   );
