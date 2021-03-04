6512fe296d9ed1edf40a386b608161053b55409a
inqueblot <notminebydesign@gmail.com>
2021-03-04 16:06:40 -0600

    set up API call

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 import axios from "axios"
 
 function App() {
+  const [employeeData, setEmployeeData] = useState([]);
 
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://randomuser.me/api/");
+      console.log(result.data)
+      setEmployeeData(result.data)
+      console.log(employeeData)
+    })();
+  }, []);
 }
 
 export default App;
